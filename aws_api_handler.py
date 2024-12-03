d3ba536c8045442eed66d87aabc8be4a1bcc94a9
Allison Dzubak <allisondzubak@Allisons-MacBook-Air.local>
2024-12-03 09:02:38 -0500

    Removed incorrect commented-out code to initialize S3 client

--- aws_api_handler.py
+++ aws_api_handler.py
@@ -5,10 +5,6 @@ import pandas as pd
 from datetime import datetime
 from io import BytesIO
 
-# # Initialize the S3 client
-# s3_client = boto3.client('s3', aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-#                          aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"))
-
 
 def get_s3_client():
     """
